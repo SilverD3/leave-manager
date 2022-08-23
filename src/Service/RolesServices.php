fabbe8a6f08d9c0ad00aa15657f1b9884facc0ec
SilverD3 <mrboornesilver@gmail.com>
2022-08-23 15:19:09 +0100

    Add Roles Services

---
+++ src/Service/RolesServices.php
@@ -0,0 +1,64 @@
+<?php
+declare(strict_types=1);
+
+namespace App\Service;
+
+require_once dirname(dirname(__DIR__)) . DS . DS . 'autoload.php';
+
+use Core\Database\ConnectionManager;
+use App\Entity\Role;
+
+/**
+ * Roles Services
+ */
+class RolesServices
+{
+    /**
+	 * @var ConnectionManager $connectionManager
+	 */
+	private $connectionManager;
+
+    /**
+     * Initializes the Roles Services by setting the connection manager
+     */
+    function __construct()
+	{
+		$this->connectionManager = new ConnectionManager();
+	}
+
+    /**
+	 * Get All Roles
+	 * @return array<Role>  Array of Roles or empty array
+	 * @throw \Exception When error occurs
+	 */
+	public function getAll()
+	{
+		$result = [];
+		$roles = [];
+
+        $sql = "SELECT * FROM roles";
+
+        try {
+            $query = $this->connectionManager->getConnection()->prepare($sql);
+            $query->execute();
+            $result = $query->fetchAll(\PDO::FETCH_ASSOC);
+        } catch (\PDOException $e) {
+			throw new \Exception("SQL Exception: " . $e->getMessage(), 1);
+		}
+
+        if (empty($result)) {
+			return [];
+		}
+
+        foreach ($result as $row) {
+            $role = new Role();
+            $role->setId($row['id']);
+            $role->setCode($row['code']);
+            $role->setName($row['name']);
+
+            $roles[] = $role;
+        }
+
+        return $roles;
+    }
+}
