5f2ecabbe4f3eb37780e3c3af132ca569bd92572
SilverD3 <mrboornesilver@gmail.com>
2022-08-23 15:24:11 +0100

    Update DateHelper. Add method to format date (string, timestamp or object) into datetime string

--- src/View/Helpers/DateHelper.php
+++ src/View/Helpers/DateHelper.php
@@ -29,6 +29,26 @@ class DateHelper
         return date('d/m/Y', $date);
     }
 
+    /**
+     * Format date to short format
+     * @param  mixed $date Date to format
+     * @return string      Formated date
+     */
+    public static function dateTime($date): ?string
+    {
+        if (self::isTimestamp($date)) {
+            return date('d/m/Y H:i', $date);
+        }
+
+        if ($date instanceof \DateTime) {
+            $date = $date->format('Y-m-d H:i:s');
+        }
+
+        $date = strtotime($date);
+
+        return date('d/m/Y H:i', $date);
+    }
+
     private static function isTimestamp($ts)
     {
         return is_numeric($ts) && ((int) $ts == $ts);
