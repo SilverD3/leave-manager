671356b2602cc0744ee9af67dfea80047622201f
SilverD3 <mrboornesilver@gmail.com>
2022-08-27 09:04:30 +0100

    Update database script. Update contracts table and add company table

--- config/schema/db-script.sql
+++ config/schema/db-script.sql
@@ -1,29 +1,78 @@
--- MySQL dump 10.13  Distrib 8.0.29, for Linux (x86_64)
+-- phpMyAdmin SQL Dump
+-- version 5.0.4deb2ubuntu5
+-- https://www.phpmyadmin.net/
 --
--- Host: localhost    Database: leave_manager
--- ------------------------------------------------------
--- Server version	8.0.29
+-- Hôte : localhost:3306
+-- Généré le : sam. 27 août 2022 à 09:01
+-- Version du serveur :  8.0.29
+-- Version de PHP : 8.0.8
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
 /*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!50503 SET NAMES utf8mb4 */;
-/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
-/*!40103 SET TIME_ZONE='+00:00' */;
-/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
-/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
-/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
-/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+/*!40101 SET NAMES utf8mb4 */;
 
 --
--- Table structure for table `contract_models`
+-- Base de données : `leave_manager`
 --
 
-DROP TABLE IF EXISTS `contract_models`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!50503 SET character_set_client = utf8mb4 */;
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `company`
+--
+
+CREATE TABLE `company` (
+  `id` int NOT NULL,
+  `name` varchar(255) COLLATE utf8mb4_general_ci NOT NULL,
+  `director_name` varchar(50) COLLATE utf8mb4_general_ci NOT NULL,
+  `address` varchar(255) COLLATE utf8mb4_general_ci NOT NULL,
+  `tel1` varchar(30) COLLATE utf8mb4_general_ci DEFAULT NULL,
+  `tel2` varchar(30) COLLATE utf8mb4_general_ci DEFAULT NULL,
+  `email` varchar(100) COLLATE utf8mb4_general_ci DEFAULT NULL,
+  `logo` varchar(255) COLLATE utf8mb4_general_ci DEFAULT NULL,
+  `about` text COLLATE utf8mb4_general_ci,
+  `created` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `modified` datetime DEFAULT NULL,
+  `modified_by` int DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `contracts`
+--
+
+CREATE TABLE `contracts` (
+  `id` int NOT NULL,
+  `employee_id` int NOT NULL,
+  `title` varchar(255) COLLATE utf8mb4_general_ci DEFAULT NULL,
+  `contract_type_id` int NOT NULL,
+  `start_date` date NOT NULL,
+  `end_date` date DEFAULT NULL,
+  `job_object` varchar(300) COLLATE utf8mb4_general_ci DEFAULT NULL,
+  `job_description` varchar(500) COLLATE utf8mb4_general_ci DEFAULT NULL,
+  `job_salary` decimal(13,4) DEFAULT NULL,
+  `hourly_rate` varchar(255) COLLATE utf8mb4_general_ci DEFAULT NULL,
+  `created` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `modified` datetime DEFAULT NULL,
+  `status` varchar(15) COLLATE utf8mb4_general_ci NOT NULL,
+  `etat` tinyint(1) NOT NULL DEFAULT '1'
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `contract_models`
+--
+
 CREATE TABLE `contract_models` (
-  `id` int NOT NULL AUTO_INCREMENT,
+  `id` int NOT NULL,
   `contract_type_id` int NOT NULL,
   `name` varchar(300) COLLATE utf8mb4_general_ci NOT NULL,
   `content` text COLLATE utf8mb4_general_ci NOT NULL,
@@ -31,92 +80,31 @@ CREATE TABLE `contract_models` (
   `status` varchar(10) COLLATE utf8mb4_general_ci NOT NULL,
   `created` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
   `modified` datetime DEFAULT NULL,
-  `etat` tinyint(1) NOT NULL DEFAULT '1',
-  PRIMARY KEY (`id`),
-  KEY `fk_contract_type_id_contract_models` (`contract_type_id`),
-  CONSTRAINT `fk_contract_type_id_contract_models` FOREIGN KEY (`contract_type_id`) REFERENCES `contract_types` (`id`)
+  `etat` tinyint(1) NOT NULL DEFAULT '1'
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
-/*!40101 SET character_set_client = @saved_cs_client */;
+
+-- --------------------------------------------------------
 
 --
--- Dumping data for table `contract_models`
+-- Structure de la table `contract_types`
 --
 
-LOCK TABLES `contract_models` WRITE;
-/*!40000 ALTER TABLE `contract_models` DISABLE KEYS */;
-/*!40000 ALTER TABLE `contract_models` ENABLE KEYS */;
-UNLOCK TABLES;
-
---
--- Table structure for table `contract_types`
---
-
-DROP TABLE IF EXISTS `contract_types`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `contract_types` (
-  `id` int NOT NULL AUTO_INCREMENT,
+  `id` int NOT NULL,
   `name` varchar(300) COLLATE utf8mb4_general_ci NOT NULL,
   `description` varchar(1000) COLLATE utf8mb4_general_ci DEFAULT NULL,
   `created` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
-  `etat` tinyint(1) NOT NULL DEFAULT '1',
-  PRIMARY KEY (`id`)
+  `etat` tinyint(1) NOT NULL DEFAULT '1'
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
-/*!40101 SET character_set_client = @saved_cs_client */;
+
+-- --------------------------------------------------------
 
 --
--- Dumping data for table `contract_types`
+-- Structure de la table `employees`
 --
 
-LOCK TABLES `contract_types` WRITE;
-/*!40000 ALTER TABLE `contract_types` DISABLE KEYS */;
-/*!40000 ALTER TABLE `contract_types` ENABLE KEYS */;
-UNLOCK TABLES;
-
---
--- Table structure for table `contracts`
---
-
-DROP TABLE IF EXISTS `contracts`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!50503 SET character_set_client = utf8mb4 */;
-CREATE TABLE `contracts` (
-  `id` int NOT NULL AUTO_INCREMENT,
-  `employee_id` int NOT NULL,
-  `title` varchar(255) COLLATE utf8mb4_general_ci DEFAULT NULL,
-  `contract_type_id` int NOT NULL,
-  `start_date` date NOT NULL,
-  `end_date` date DEFAULT NULL,
-  `created` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
-  `modified` datetime DEFAULT NULL,
-  `status` varchar(15) COLLATE utf8mb4_general_ci NOT NULL,
-  `etat` tinyint(1) NOT NULL DEFAULT '1',
-  PRIMARY KEY (`id`),
-  KEY `fk_employee_id_contracts` (`employee_id`),
-  KEY `fk_contract_type_id_contracts` (`contract_type_id`),
-  CONSTRAINT `fk_contract_type_id_contracts` FOREIGN KEY (`contract_type_id`) REFERENCES `contract_types` (`id`),
-  CONSTRAINT `fk_employee_id_contracts` FOREIGN KEY (`employee_id`) REFERENCES `employees` (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
-/*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Dumping data for table `contracts`
---
-
-LOCK TABLES `contracts` WRITE;
-/*!40000 ALTER TABLE `contracts` DISABLE KEYS */;
-/*!40000 ALTER TABLE `contracts` ENABLE KEYS */;
-UNLOCK TABLES;
-
---
--- Table structure for table `employees`
---
-
-DROP TABLE IF EXISTS `employees`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `employees` (
-  `id` int NOT NULL AUTO_INCREMENT,
+  `id` int NOT NULL,
   `first_name` varchar(50) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL,
   `last_name` varchar(50) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL,
   `email` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL,
@@ -128,32 +116,23 @@ CREATE TABLE `employees` (
   `token` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci DEFAULT NULL,
   `token_exp_date` datetime DEFAULT NULL,
   `status` varchar(10) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL,
-  `etat` tinyint(1) NOT NULL DEFAULT '1',
-  PRIMARY KEY (`id`),
-  KEY `fk_role_id_employees` (`role_id`),
-  CONSTRAINT `fk_role_id_employees` FOREIGN KEY (`role_id`) REFERENCES `roles` (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
-/*!40101 SET character_set_client = @saved_cs_client */;
+  `etat` tinyint(1) NOT NULL DEFAULT '1'
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
--- Dumping data for table `employees`
+-- Déchargement des données de la table `employees`
 --
 
-LOCK TABLES `employees` WRITE;
-/*!40000 ALTER TABLE `employees` DISABLE KEYS */;
 INSERT INTO `employees` VALUES (1,'Admin','admin','admin@gmail.com','admin','4ceb415ca0376305106660973f6f9cf550126cb0fc2a9fdcff1b30b4e6e27383 ',1,'2022-08-05 16:19:18',NULL,NULL,NULL,'pending',1);
-/*!40000 ALTER TABLE `employees` ENABLE KEYS */;
-UNLOCK TABLES;
+
+-- --------------------------------------------------------
 
 --
--- Table structure for table `leaves`
+-- Structure de la table `leaves`
 --
 
-DROP TABLE IF EXISTS `leaves`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `leaves` (
-  `id` int NOT NULL AUTO_INCREMENT,
+  `id` int NOT NULL,
   `employee_id` int NOT NULL,
   `year` int NOT NULL,
   `days` int NOT NULL,
@@ -162,31 +141,34 @@ CREATE TABLE `leaves` (
   `created` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
   `modified` datetime DEFAULT NULL,
   `note` varchar(1000) COLLATE utf8mb4_general_ci DEFAULT NULL,
-  `etat` tinyint(1) NOT NULL DEFAULT '1',
-  PRIMARY KEY (`id`),
-  KEY `fk_employee_id_leaves` (`employee_id`),
-  CONSTRAINT `fk_employee_id_leaves` FOREIGN KEY (`employee_id`) REFERENCES `employees` (`id`)
+  `etat` tinyint(1) NOT NULL DEFAULT '1'
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
-/*!40101 SET character_set_client = @saved_cs_client */;
+
+-- --------------------------------------------------------
 
 --
--- Dumping data for table `leaves`
+-- Structure de la table `permissions`
 --
 
-LOCK TABLES `leaves` WRITE;
-/*!40000 ALTER TABLE `leaves` DISABLE KEYS */;
-/*!40000 ALTER TABLE `leaves` ENABLE KEYS */;
-UNLOCK TABLES;
+CREATE TABLE `permissions` (
+  `id` int NOT NULL,
+  `employee_id` int NOT NULL,
+  `reason` varchar(500) COLLATE utf8mb4_general_ci NOT NULL,
+  `description` varchar(1000) COLLATE utf8mb4_general_ci DEFAULT NULL,
+  `start_date` date NOT NULL,
+  `end_date` date NOT NULL,
+  `created` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `etat` tinyint(1) NOT NULL DEFAULT '1'
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
 
 --
--- Table structure for table `permission_requests`
+-- Structure de la table `permission_requests`
 --
 
-DROP TABLE IF EXISTS `permission_requests`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `permission_requests` (
-  `id` int NOT NULL AUTO_INCREMENT,
+  `id` int NOT NULL,
   `employee_id` int NOT NULL,
   `reason` varchar(500) COLLATE utf8mb4_general_ci NOT NULL,
   `description` varchar(1000) COLLATE utf8mb4_general_ci NOT NULL,
@@ -194,85 +176,194 @@ CREATE TABLE `permission_requests` (
   `end_date` date NOT NULL,
   `status` varchar(10) COLLATE utf8mb4_general_ci NOT NULL,
   `created` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
-  `etat` tinyint(1) NOT NULL DEFAULT '1',
-  PRIMARY KEY (`id`),
-  KEY `fk_employee_id_permission_requests` (`employee_id`),
-  CONSTRAINT `fk_employee_id_permission_requests` FOREIGN KEY (`employee_id`) REFERENCES `employees` (`id`)
+  `etat` tinyint(1) NOT NULL DEFAULT '1'
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
-/*!40101 SET character_set_client = @saved_cs_client */;
+
+-- --------------------------------------------------------
 
 --
--- Dumping data for table `permission_requests`
+-- Structure de la table `roles`
 --
 
-LOCK TABLES `permission_requests` WRITE;
-/*!40000 ALTER TABLE `permission_requests` DISABLE KEYS */;
-/*!40000 ALTER TABLE `permission_requests` ENABLE KEYS */;
-UNLOCK TABLES;
-
---
--- Table structure for table `permissions`
---
-
-DROP TABLE IF EXISTS `permissions`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!50503 SET character_set_client = utf8mb4 */;
-CREATE TABLE `permissions` (
-  `id` int NOT NULL AUTO_INCREMENT,
-  `employee_id` int NOT NULL,
-  `reason` varchar(500) COLLATE utf8mb4_general_ci NOT NULL,
-  `description` varchar(1000) COLLATE utf8mb4_general_ci DEFAULT NULL,
-  `start_date` date NOT NULL,
-  `end_date` date NOT NULL,
-  `created` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
-  `etat` tinyint(1) NOT NULL DEFAULT '1',
-  PRIMARY KEY (`id`),
-  KEY `fk_remployee_id_permissions` (`employee_id`),
-  CONSTRAINT `fk_remployee_id_permissions` FOREIGN KEY (`employee_id`) REFERENCES `employees` (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
-/*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Dumping data for table `permissions`
---
-
-LOCK TABLES `permissions` WRITE;
-/*!40000 ALTER TABLE `permissions` DISABLE KEYS */;
-/*!40000 ALTER TABLE `permissions` ENABLE KEYS */;
-UNLOCK TABLES;
-
---
--- Table structure for table `roles`
---
-
-DROP TABLE IF EXISTS `roles`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `roles` (
-  `id` int NOT NULL AUTO_INCREMENT,
+  `id` int NOT NULL,
   `code` varchar(3) COLLATE utf8mb4_general_ci NOT NULL,
-  `name` varchar(15) COLLATE utf8mb4_general_ci NOT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
-/*!40101 SET character_set_client = @saved_cs_client */;
+  `name` varchar(15) COLLATE utf8mb4_general_ci NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
--- Dumping data for table `roles`
+-- Déchargement des données de la table `roles`
 --
 
-LOCK TABLES `roles` WRITE;
-/*!40000 ALTER TABLE `roles` DISABLE KEYS */;
-INSERT INTO `roles` VALUES (1,'ADM','Admin'),(2,'EMP','Employee');
-/*!40000 ALTER TABLE `roles` ENABLE KEYS */;
-UNLOCK TABLES;
-/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+INSERT INTO `roles` (`id`, `code`, `name`) VALUES
+(1, 'ADM', 'Admin'),
+(2, 'EMP', 'Employee');
 
-/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
-/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
-/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+--
+-- Index pour les tables déchargées
+--
+
+--
+-- Index pour la table `company`
+--
+ALTER TABLE `company`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Index pour la table `contracts`
+--
+ALTER TABLE `contracts`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `fk_employee_id_contracts` (`employee_id`),
+  ADD KEY `fk_contract_type_id_contracts` (`contract_type_id`);
+
+--
+-- Index pour la table `contract_models`
+--
+ALTER TABLE `contract_models`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `fk_contract_type_id_contract_models` (`contract_type_id`);
+
+--
+-- Index pour la table `contract_types`
+--
+ALTER TABLE `contract_types`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Index pour la table `employees`
+--
+ALTER TABLE `employees`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `fk_role_id_employees` (`role_id`);
+
+--
+-- Index pour la table `leaves`
+--
+ALTER TABLE `leaves`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `fk_employee_id_leaves` (`employee_id`);
+
+--
+-- Index pour la table `permissions`
+--
+ALTER TABLE `permissions`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `fk_remployee_id_permissions` (`employee_id`);
+
+--
+-- Index pour la table `permission_requests`
+--
+ALTER TABLE `permission_requests`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `fk_employee_id_permission_requests` (`employee_id`);
+
+--
+-- Index pour la table `roles`
+--
+ALTER TABLE `roles`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT pour les tables déchargées
+--
+
+--
+-- AUTO_INCREMENT pour la table `company`
+--
+ALTER TABLE `company`
+  MODIFY `id` int NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT pour la table `contracts`
+--
+ALTER TABLE `contracts`
+  MODIFY `id` int NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT pour la table `contract_models`
+--
+ALTER TABLE `contract_models`
+  MODIFY `id` int NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT pour la table `contract_types`
+--
+ALTER TABLE `contract_types`
+  MODIFY `id` int NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT pour la table `employees`
+--
+ALTER TABLE `employees`
+  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- AUTO_INCREMENT pour la table `leaves`
+--
+ALTER TABLE `leaves`
+  MODIFY `id` int NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT pour la table `permissions`
+--
+ALTER TABLE `permissions`
+  MODIFY `id` int NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT pour la table `permission_requests`
+--
+ALTER TABLE `permission_requests`
+  MODIFY `id` int NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT pour la table `roles`
+--
+ALTER TABLE `roles`
+  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- Contraintes pour les tables déchargées
+--
+
+--
+-- Contraintes pour la table `contracts`
+--
+ALTER TABLE `contracts`
+  ADD CONSTRAINT `fk_contract_type_id_contracts` FOREIGN KEY (`contract_type_id`) REFERENCES `contract_types` (`id`),
+  ADD CONSTRAINT `fk_employee_id_contracts` FOREIGN KEY (`employee_id`) REFERENCES `employees` (`id`);
+
+--
+-- Contraintes pour la table `contract_models`
+--
+ALTER TABLE `contract_models`
+  ADD CONSTRAINT `fk_contract_type_id_contract_models` FOREIGN KEY (`contract_type_id`) REFERENCES `contract_types` (`id`);
+
+--
+-- Contraintes pour la table `employees`
+--
+ALTER TABLE `employees`
+  ADD CONSTRAINT `fk_role_id_employees` FOREIGN KEY (`role_id`) REFERENCES `roles` (`id`);
+
+--
+-- Contraintes pour la table `leaves`
+--
+ALTER TABLE `leaves`
+  ADD CONSTRAINT `fk_employee_id_leaves` FOREIGN KEY (`employee_id`) REFERENCES `employees` (`id`);
+
+--
+-- Contraintes pour la table `permissions`
+--
+ALTER TABLE `permissions`
+  ADD CONSTRAINT `fk_remployee_id_permissions` FOREIGN KEY (`employee_id`) REFERENCES `employees` (`id`);
+
+--
+-- Contraintes pour la table `permission_requests`
+--
+ALTER TABLE `permission_requests`
+  ADD CONSTRAINT `fk_employee_id_permission_requests` FOREIGN KEY (`employee_id`) REFERENCES `employees` (`id`);
+COMMIT;
+
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
-/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
-
--- Dump completed on 2022-08-09 11:24:13
