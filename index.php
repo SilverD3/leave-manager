54a9716dbcafbdc8d30ddeb5ea22e6cfcf2333c6
SilverD3 <mrboornesilver@gmail.com>
2022-08-30 11:33:20 +0100

    Remove debugging in index.php

--- index.php
+++ index.php
@@ -1,9 +1,5 @@
 <?php
 
-ini_set('display_errors', 1);
-ini_set('display_startup_errors', 1);
-error_reporting(E_ALL);
-
 require_once __DIR__ . DIRECTORY_SEPARATOR . 'autoload.php';
 
 require_once APP_DIR . DS . 'View' . DS . 'Employees' . DS . 'dashboard.php';
