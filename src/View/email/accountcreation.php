582c5a729a02378973507d17b9b7a802ba497aa5
SilverD3 <mrboornesilver@gmail.com>
2024-07-24 17:15:07 +0100

    chore:add notification on user account creation

---
+++ src/View/email/accountcreation.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <title>Crétation de votre compte d'utilisateur | LeaveManager</title>
+</head>
+
+<body>
+    <p>Votre compte d'utilisateur a été créé </p>
+
+    <p>
+        Veuillez vous rendre sur l'
+        <a href="$_app_link" style="text-decoration: none;">
+            application
+        </a>
+        pour vous connecter et profiter de votre compte
+    </p>
+
+    <p style="margin-top: 20px;">Vos paramètres de connexion sont les suivant:</p>
+
+    <p style=" margin-top: 5px; margin-left: 15px;">
+        <span style="font-weight: bold; margin-bottom: 10px;">Adresse e-mail: $_user_email </span><br />
+        <span style="font-weight: bold;">Mot de passe: $_user_pwd </span>
+    </p>
+
+    <p style=" font-size: 9px; text-align: center; padding: 10px; background-color: rgba(10, 10, 10, 0.2); margin-top:80px;">
+            Vous receivez ce mail parce qu'une activité en relation avec votre compte a été détectée.
+    </p>
+</body>
+
+</html>
